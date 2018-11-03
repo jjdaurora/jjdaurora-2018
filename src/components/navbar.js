2ea1eebedad4095feb902bc1248da8bfa796601b
JJ D'Aurora <jjdaurora@gmail.com>
2018-11-03 11:45:25 -0400

    working on navbar fixes

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,20 +2,30 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const NavBar = ({ siteTitle }) => (
-    <nav>
-      <h1><Link to="/">{siteTitle}</Link></h1>
-        <ul>
-          <li>
-            <Link to="/about/">About</Link>
-          </li>
-          <li>
-            <Link to="/projects/">Projects</Link>
-          </li>
-          <li>
-            <Link to="/contact/">Contact</Link>
-          </li>
-        </ul>
-    </nav>
+    // <nav>
+    //   <h1><Link to="/">{siteTitle}</Link></h1>
+    //     <ul>
+    //       <li>
+    //         <Link to="/about/">About</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/projects/">Projects</Link>
+    //       </li>
+    //       <li>
+    //         <Link to="/contact/">Contact</Link>
+    //       </li>
+    //     </ul>
+    // </nav>
+  <nav>
+    <ul className="nav-container">
+      <li>Home</li>
+      <li>Profile</li>
+      <li class="search">
+        <input type="text" class="search-input" placeholder="Search"/>
+    </li>
+        <li>Logout</li>
+   </ul>
+</nav>
 )
 
 export default NavBar
