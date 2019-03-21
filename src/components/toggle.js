b333571982a22a4573d6047f5bd21123076c48d8
JJ D'Aurora <jjdaurora@gmail.com>
2019-03-20 21:55:58 -0400

    toggle

---
+++ src/components/toggle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Toggle = () => {
+    return (
+    <div class="row">
+        <button className="button">
+            Dark
+        </button>
+    </div>
+    )
+}
+
+export default Toggle;
