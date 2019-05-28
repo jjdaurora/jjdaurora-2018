af7d4d010f218d51dce56ddc7363a35343622b5b
JJ D'Aurora <jjdaurora@gmail.com>
2019-05-28 09:01:06 -0400

    update job

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <div className="branding-image"></div>
     <div className="home-wrap__content">
       <h1>JJ D'Aurora</h1>
-      <p>JJ D'Aurora is a full stack web developer at <a style={inlineStyle} href="https://www.brandmuscle.com">BrandMuscle</a>.</p>
+      <p>JJ D'Aurora is a full stack web developer with a passion for customer success.</p>
       <p>His work mostly centers on software product development and implementations. Most recently, he and his team developed an e-commerce shopping cart application built on Angular and .NET Core.
       He approaches his work with a strong mix of business and technical aptitude, and he is always interested in engaging in new and exciting work.</p>
       <p>When he isn't working on software, he can be found failing at his attempts in the kitchen or studying Chinese. He likes music, philosophy, walking his dog, and debating just about anything.</p>
